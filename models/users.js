9f140e5908734415a84787452d319fda1b135240
yisha0307 <chenchen9037@126.com>
2019-03-13 22:08:19 +0800

    finish signin

--- models/users.js
+++ models/users.js
@@ -1,7 +1,13 @@
 const User = require('../lib/mongo').User
 
 module.exports = {
+    // 注册一个用户
     create: function create (user) {
         return User.create(user).exec()
+    },
+    // 通过用户名获取用户信息
+    // addCreatedAt() 自定义插件（通过_id生成时间戳）
+    getUserByName: function getUserByName (name) {
+        return User.findOne({name: name}).addCreatedAt().exec()
     }
 }
--- routes/signout.js
+++ routes/signout.js
@@ -4,7 +4,11 @@ const router = express.Router()
 const checkLogin = require('../middlewares/check').checkLogin
 
 router.get('/', checkLogin, (req, res, next) => {
-    res.send('登出')
+    // 清空sesson中的用户信息
+    req.session.user = null
+    req.flash('success', '登出成功')
+    // 登出成功后跳转到主页
+    res.redirect('/posts')
 })
 
 module.exports = router
