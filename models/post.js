6d8694bd7117a5cb71419730fc53c45ad963b4c2
yisha0307 <chenchen9037@126.com>
2019-03-19 22:11:31 +0800

    add edit && remove posts

--- models/post.js
+++ models/post.js
@@ -47,9 +47,49 @@ module.exports = {
             })
         })
     },
+    // 增加点击量
     incPV: function incPV (_id) {
         return new Promise(() => {
             Post.updateOne({_id}, {$inc: {pv: 1}})
+        })
+    },
+    // 通过文章id获取一篇原生文章（编辑文章）
+    getRawPostById: function (postId) {
+        return new Promise((resolve, reject) => {
+            Post.findOne({_id: postId}).populate('author').exec((err, doc) => {
+                if (err) {
+                    console.log(err)
+                    reject(err)
+                } else {
+                    resolve(doc)
+                }
+            })
+        })
+    },
+    // 通过id更新一篇文章
+    updatePostById: function (postId, data) {
+        return new Promise((resolve, reject) => {
+            Post.updateOne({_id: postId}, {$set: data}).exec((err, doc) => {
+                if (err) {
+                    console.log(err)
+                    reject(err)
+                } else {
+                    resolve(doc)
+                }
+            })
+        })
+    },
+    // 通过id删除一篇文章
+    deletePostById: function (postId) {
+        return new Promise((resolve, reject) =>{
+            Post.deleteOne({_id: postId}).exec((err, doc) => {
+                if (err) {
+                    console.log(err)
+                    reject(err)
+                } else {
+                    resolve(doc)
+                }
+            })
         })
     }
 }
