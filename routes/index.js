e73e05a54e4bc85bd7f5ed21fae376be2cae260b
yisha0307 <chenchen9037@126.com>
2019-05-24 14:34:25 +0800

    try github

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 module.exports = function (app) {
     app.use('/user', require('./user'))
-    app.use('/signup', require('./signup'))
     app.use('/signin', require('./signin'))
     app.use('/signout', require('./signout'))
     app.use('/post', require('./posts'))
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const UserModel = require('../models/user')
 const fs = require('fs')
+const path = require('path')
 
 router.get('/initData', (req, res, next) => {
     console.log(req.session)
@@ -14,7 +15,9 @@ router.get('/initData', (req, res, next) => {
 })
 
 router.post('/signup', (req, res, next) => {
-    const {name, password, bio, avatar, gender} = req.body
+    let {name, password, bio, avatar, gender} = req.body
+    avatar = avatar.path.split(path.sep).pop()
+    console.log(avatar)
     let user = {
         name,
         password,
